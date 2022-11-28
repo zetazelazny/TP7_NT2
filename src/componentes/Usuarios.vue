45b1565481f83d773de176d56bb4bc1524ded6f2
Zeta <matiaszelazny99@gmail.com>
2022-11-28 19:19:19 -0300

    TP7

---
+++ src/componentes/Usuarios.vue
@@ -0,0 +1,104 @@
+<template >
+
+  <section class="src-componentes-usuarios">
+     <div class="jumbotron">
+      <h2>Usuarios</h2>
+      <hr />
+      <hr />
+      <br />
+
+
+      <button class="btn btn-danger my-3 mr-3" @click="getUsuariosAxios()">
+        Pedir Axios then catch
+      </button>
+
+      <button class="btn btn-danger my-3 mr-3" @click="getUsuariosFetchAsync()">
+        Pedir Axios async await
+      </button>
+
+      <button class="btn btn-info my-3 mr-3" @click="usuarios = []">
+        Limpiar
+      </button>
+      <br />
+
+      <div v-if="usuarios.length" class="table-responsive">
+        <table class="table table-dark">
+          <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Edad</th>
+            <th>Email</th>            
+          </tr>          
+          <tr v-for="usuario in usuarios" :key="usuario.id">
+            <td>{{ usuario.id }}</td>
+            <td>{{ usuario.nombre }}</td>
+            <td>{{ usuario.edad }}</td>            
+            <td>{{ usuario.email }}</td>            
+          </tr>
+        </table>
+      </div>
+      <h4 v-else class="alert alert-info text-center">
+        No se encuentran datos
+      </h4>
+    </div>
+  </section>
+
+</template>
+
+<script >
+
+  export default  {
+    name: 'src-componentes-usuarios',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        usuarios : [],
+        url : 'https://6372d0af348e947299fd572c.mockapi.io/users_tp7',        
+      }
+    },
+    methods: {
+        async getUsuariosFetchAsync() {                
+          try {
+            let response = await fetch(this.url)        
+            let respuesta = await response.json();       
+            this.usuarios = respuesta;
+          }
+          catch(error) {
+            console.log(error);
+          }          
+
+            
+        },  
+
+        getUsuariosAxios() {          
+        this.axios(this.url)
+          .then( respuesta => {this.usuarios = respuesta.data })
+          .catch((error) => console.log(error))       
+      },
+      },   
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-componentes-usuarios {
+
+  }
+
+  .jumbotron {
+    background-color: rgb(246, 218, 218);
+    color: rgb(15, 17, 59);
+  }
+
+  hr {
+    background-color: #bbb;
+  }
+
+</style>
